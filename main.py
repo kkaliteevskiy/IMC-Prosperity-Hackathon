68cc57cc41eeafcd9cbd2cc853ba9593b5f2a339
kkaliteevskiy <kirill.kaliteevskiy@gmail.com>
2023-03-22 18:13:08 +0000

    added a day 2 directory

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import plotly.express as px
 df =  pd.read_csv('day_1_data\\trades_round_1_day_0_nn.csv', delimiter = ';')# Read csv file into dataframe
 
 #%%
+df = df[df.symbol == 'BANANAS']
+
+df['price_roll_avr'] = df.rolling(50)['price'].mean()
+
+
+#%%
 #plot pricess
 
 fig = px.line(df, x = 'timestamp', y = 'price', color = 'symbol')#create plot
