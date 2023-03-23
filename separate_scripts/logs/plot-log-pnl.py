1176c1d398e1cbe70e124b50e9602f594b751499
a-haber <98184411+a-haber@users.noreply.github.com>
2023-03-23 00:11:20 +0000

    improved data visualisation

---
+++ separate_scripts/logs/plot-log-pnl.py
@@ -0,0 +1,13 @@
+# program reads a log file and plots the cumulative PnL (profit and loss) with time 
+# assuming the file is a .txt which starts after the 'Activities log:' (ie. no console outputs)
+
+import pandas as pd
+import plotly.express as px  
+
+# ***change location of file here***
+filename = 'log1.txt'
+df =  pd.read_csv(f'separate_scripts\\logs\\{filename}', delimiter = ';') # Read csv file into dataframe
+
+# plot price history
+fig = px.line(df, x = 'timestamp', y = 'profit_and_loss', color = 'product', title = f'file: {filename}')
+fig.show(renderer="browser")
